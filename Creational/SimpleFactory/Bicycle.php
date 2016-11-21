c53ab5b3a99a8948a18d65a201fc5c6b44f1178b
chrisye909 <yjx909@sina.com>
2016-11-21 21:13:07 +0800

    简单工厂笔记

--- Creational/SimpleFactory/Bicycle.php
+++ Creational/SimpleFactory/Bicycle.php
@@ -2,6 +2,9 @@
 
 namespace DesignPatterns\Creational\SimpleFactory;
 
+/**
+ * 一个具体的类，需要被生成的类
+ */
 class Bicycle
 {
     public function driveTo(string $destination)
--- Creational/SimpleFactory/Tests/SimpleFactoryTest.php
+++ Creational/SimpleFactory/Tests/SimpleFactoryTest.php
@@ -6,6 +6,10 @@ use DesignPatterns\Creational\SimpleFactory\SimpleFactory;
 
 class SimpleFactoryTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * 使用简单工厂需要先实例化一个简单工厂，调用方法生成需要的对象。
+     * @return [type] [description]
+     */
     public function testCanCreateBicycle()
     {
         $bicycle = (new SimpleFactory())->createBicycle();
