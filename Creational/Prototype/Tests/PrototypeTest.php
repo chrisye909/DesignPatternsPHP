4e9ca7fe6e928efd4319f3d2aa8b86b98cc5e8b6
chrisye909 <yjx909@sina.com>
2016-11-21 22:24:13 +0800

    原型模式笔记

--- Creational/Prototype/Tests/PrototypeTest.php
+++ Creational/Prototype/Tests/PrototypeTest.php
@@ -7,12 +7,20 @@ use DesignPatterns\Creational\Prototype\FooBookPrototype;
 
 class PrototypeTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * 通过创建原型使用克隆方法实现对象创建而不是使用标准的 new 方式。
+     * 几种使用场景：
+     * 1. 如果说我们的对象类型不是刚开始就能确定，而是在运行时确定的话，那么我们通过这个类型的对象克隆出一个新的类型更容易。
+     * 2. 有的时候我们可能在实际的项目中需要一个对象在某个状态下的副本，这个前提很重要，这点怎么理解呢，例如有的时候我们需要对比一个对象经过处理后的状态和处理前的状态是否发生过改变，可能我们就需要在执行某段处理之前，克隆这个对象此时状态的副本，然后等执行后的状态进行相应的对比，这样的应用在项目中也是经常会出现的。
+     * 3. 当我们处理的对象比较简单，并且对象之间的区别很小，可能只是很固定的几个属性不同的时候，使用原型模式更合适。
+     */
     public function testCanGetFooBook()
     {
         $fooPrototype = new FooBookPrototype();
         $barPrototype = new BarBookPrototype();
 
         for ($i = 0; $i < 10; $i++) {
+            // 通过复制获取对象而非实例化
             $book = clone $fooPrototype;
             $book->setTitle('Foo Book No ' . $i);
             $this->assertInstanceOf('DesignPatterns\Creational\Prototype\FooBookPrototype', $book);
