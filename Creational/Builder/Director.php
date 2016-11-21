454af1f92abb8b05536e08a7c64dd91dd8d0a69d
chrisye909 <yjx909@sina.com>
2016-11-21 22:09:45 +0800

    建造者模式笔记

--- Creational/Builder/Director.php
+++ Creational/Builder/Director.php
@@ -10,6 +10,10 @@ use DesignPatterns\Creational\Builder\Parts\Vehicle;
  *
  * You can also inject many builders instead of one to build more complex objects
  */
+/**
+ * 建造者模式将一个复杂的对象的构建与它的表示分离，使得同样的构建过程可以创建不同的表示。
+ * 建造者模式的 Director 负责组建各个部分。而每个部分的生成交由各自的 Builder 类建造。
+ */
 class Director
 {
     public function build(BuilderInterface $builder): Vehicle
